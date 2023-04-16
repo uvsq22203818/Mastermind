ed939adbf8a597ad5e5aa6442f37af2f4f08b31a
Romain THOMAS <romain.thomas@ens.uvsq.fr>
2023-04-16 23:44:54 +0200

    sauvegarde de parties

--- traitement_texte.py
+++ traitement_texte.py
@@ -9,7 +9,13 @@ essais_max = 10
 combinaison = []
 tentative = []
 nbr_tentative = 0
+liste_rond = []
 
+def supprimer_ronds():
+    global liste_rond
+    for rond in liste_rond:
+        plateau.delete(rond)
+    liste_rond =[]
 
 def affichage_cercle():
     
@@ -20,50 +26,46 @@ def affichage_cercle():
     coor_horiz_pt2 = 40
     coor_vert_pt2 = 460-50*(nbr_tentative)
     for i in range (len(tentative)) :
-        plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
+        rond = plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
                             coor_horiz_pt2, coor_vert_pt2, fill=tentative[couleur_cercle])
+        liste_rond.append(rond)
         coor_horiz_pt1 += 40
         coor_horiz_pt2 += 40
         couleur_cercle += 1
 
 # fonction qui interprète chaques tentatives
 def verification():
-    if nbr_tentative <= 10 :
-        global tentative
-        if tentative == combinaison :
-            fenetre_victoire = tk.Tk()
-            canvas = tk.Canvas(fenetre_victoire, width=300, height=200)
-            canvas.grid()
-            text = canvas.create_text(150, 50, text="YOU WIN <3 !!!")
-        else :
-            # ici, on a crée les conditions pour lesquelles
-            # on va afficher les cercles qui donnent des indices 
-            nb_bienplace= 0
-            nb_malplace= 0
-            for i in range(taille_code):
-                if combinaison[i]==tentative[i]:# on verifie que la couleur est bien placé
-                    nb_bienplace+=1 # on compte le nombre de couleur bien placé
-                    tentative[i]="x" # on modifie couleur bien placé pour pas qu'elle soit compté plusieurs fois
-                else:
-                    for j in range(len(tentative)):
-                        if combinaison[i]==tentative[j]:# on regarde si les couleurs sont mal placé
-                            nb_malplace+=1 # on compte le nombre de couleur mal placé
-                            tentative[j]="x" # on modifie couleur mal placé pour pas qu'elle soit compté plusieurs fois
-                            break # on sort de la boucle une fois qu'une couleur mal placé ait été compté
-                                  # pour une couleur du code
-            
-            cercle_de_vérif(nb_bienplace, nb_malplace)
-
-            if nbr_tentative==10:
-                fenetre_defaite = tk.Tk()
-                canvas = tk.Canvas(fenetre_defaite, width=300, height=200)
-                canvas.grid()
-                text = canvas.create_text(150, 50, text="YOU LOOSE :(")
-
-            print(nb_bienplace)
-            print(nb_malplace)
-                            
-            tentative = []
+    
+    # ici, on a crée les conditions pour lesquelles
+    # on va afficher les cercles qui donnent des indices 
+    global tentative
+    nb_bienplace= 0
+    nb_malplace= 0
+    copi_combi= combinaison.copy()
+    for i in range(len(combinaison)):
+        if combinaison[i]==tentative[i]:# on verifie que la couleur est bien placé
+            nb_bienplace+=1 # on compte le nombre de couleur bien placé
+            copi_combi[i] = "x" # on modifie couleur bien placé pour pas qu'elle soit compté plusieurs fois
+            tentative[i] = "y"
+        
+    for j in range(len(combinaison)):
+        for k in range(len(tentative)):
+            if copi_combi[j] == tentative[k]:# on regarde si les couleurs sont mal placé
+                nb_malplace+=1 # on compte le nombre de couleur mal placé
+                tentative[k] = "y" # on modifie couleur mal placé pour pas qu'elle soit compté plusieurs fois
+                break # on sort de la boucle une fois qu'une couleur mal placé soit compté une seule fois
+                      
+    cercle_de_vérif(nb_bienplace, nb_malplace)
+    
+    if nb_bienplace == 4 :
+        afficher_message("Vous avez gagné !!")
+        effacer_boutons_couleur()
+        afficher_bouton_rejouer()
+    elif nbr_tentative==10:
+        afficher_message("Vous avez perdu :/")
+        effacer_boutons_couleur()
+        afficher_bouton_rejouer()
+    tentative = []
     
 
 # fonction qui affiche les cercles qui donnent des indices
@@ -74,8 +76,9 @@ def cercle_de_vérif(a,b):
     coor_horiz_pt2 = 195
     coor_vert_pt2 = 482.5-50*(nbr_tentative-1)
     for i in range (a):
-        plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
+        rond = plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
                             coor_horiz_pt2, coor_vert_pt2, fill="red")
+        liste_rond.append(rond)
         coor_horiz_pt1 += 30
         coor_horiz_pt2 += 30
 
@@ -84,23 +87,24 @@ def cercle_de_vérif(a,b):
     coor_horiz_pt2 = 195+30*a
     coor_vert_pt2 = 482.5-50*(nbr_tentative-1)
     for i in range (b):
-        plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
+        rond = plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
                             coor_horiz_pt2, coor_vert_pt2, fill="white")
+        liste_rond.append(rond)
         coor_horiz_pt1 += 30
         coor_horiz_pt2 += 30
-
-# def tentative_joueur():
     
-    
-#     global nbr_tentative
-#     nbr_tentative +=1
-#     print(tentative)
-#     affichage_cercle()
-#     verification()
 
 def clic_bouton(color):
-    tentative.append(color)
-    affichage_cercle()
+    
+    if len(combinaison)<taille_code:
+        combinaison.append(color)
+        if len(combinaison)==4:
+            
+            print(combinaison)
+    else:
+        tentative.append(color)
+        affichage_cercle()
+    
     if len(tentative)==taille_code:
         global nbr_tentative
         nbr_tentative +=1
@@ -123,11 +127,89 @@ def clic_bleu():
 
 def clic_jaune():
     clic_bouton("yellow")        
-               
-                
-               
-               
+
+def clic_rejouer():
+    supprimer_ronds()
+    global combinaison
+    combinaison = []
+    global tentative
+    tentative = []
+    global nbr_tentative
+    nbr_tentative = 0
+    afficher_choix_mode()
+    effacer_bouton_rejouer()
+    effacer_message()
+
+def effacer_bouton_rejouer():
+    plateau.delete(w_rejouer)
         
+
+
+def afficher_bouton_rejouer():
+    bouton_rejouer = tk.Button(root, text="rejouer", command= clic_rejouer) 
+    global w_rejouer
+    w_rejouer = plateau.create_window(50, 150, window= bouton_rejouer)
+
+def effacer_boutons_couleur():
+    choix_couleurs.delete(w_red)
+    choix_couleurs.delete(w_green)
+    choix_couleurs.delete(w_orange)
+    choix_couleurs.delete(w_purple)
+    choix_couleurs.delete(w_blue)
+    choix_couleurs.delete(w_yellow)
+
+def afficher_boutons_couleurs():
+    # boutons de courleurs:
+    bouton_red = tk.Button(root, text="rouge", background="red", command= clic_rouge) 
+    global w_red
+    w_red = choix_couleurs.create_window(50, 150, window= bouton_red)
+
+    bouton_green = tk.Button(root, text="vert", background="green", command= clic_vert) 
+    global w_green
+    w_green= choix_couleurs.create_window(50, 200, window= bouton_green)
+
+    bouton_blue = tk.Button(root, text="bleu", background="blue", command= clic_bleu) 
+    global w_blue
+    w_blue = choix_couleurs.create_window(50, 250, window= bouton_blue)
+
+    bouton_yellow = tk.Button(root, text="jaune", background="yellow", command= clic_jaune) 
+    global w_yellow
+    w_yellow = choix_couleurs.create_window(50, 300, window= bouton_yellow)
+
+    bouton_orange = tk.Button(root, text="orange", background="orange", command= clic_orange) 
+    global w_orange
+    w_orange = choix_couleurs.create_window(50, 350, window= bouton_orange)
+
+    bouton_purple = tk.Button(root, text="violet", background="purple", command= clic_violet) 
+    global w_purple
+    w_purple = choix_couleurs.create_window(50, 400, window= bouton_purple)
+    
+
+def effacer_choix_mode():
+    mode_jeu.delete(text)
+    mode_jeu.delete(w1)
+    mode_jeu.delete(w2)
+
+def afficher_choix_mode():
+    global text
+    text = mode_jeu.create_text(150, 30, text="Choisissez un mode :")# text qui demande de choisir un mode de jeu
+    global bouton1joueur
+    global bouton2joueur
+    bouton1joueur = tk.Button(root, text="1 joueur", command= joueur1_utilise)# bouton pour jouer à 1 joueur
+    bouton2joueur = tk.Button(root, text="2 joueurs", command= joueur2_utilise)# bouton pour jouer à 2 joueurs
+    
+    global w1
+    global w2
+    w1 = mode_jeu.create_window(100, 70, window=bouton1joueur)# emplacement du bouton "1 joueur"
+    w2 = mode_jeu.create_window(200, 70, window=bouton2joueur)# emplacement du bouton "2 joueurs"
+
+def afficher_message(message):
+    label_message = tk.Label(root, padx=15, pady=30, text= message, font = ("helvetica", "10"))
+    global w_label_message
+    w_label_message = mode_jeu.create_window(50, 30, window= label_message)
+
+def effacer_message():
+    mode_jeu.delete(w_label_message)
 
 # Permet de générer un code aléatoirement
 # Fonction appelé par l'utilisation du bouton "1 joueur" dans l'affichage menu
@@ -138,15 +220,14 @@ def joueur1_utilise():
     print(combinaison)
     tentative = []
     nbr_tentative= 0
-    
+    effacer_choix_mode()
+    afficher_boutons_couleurs()
 # Permet à un deuxième joueur de choisir un code secret.
 # Fonction appelé par l'utilisation du bouton "2 joueur" dans l'affichage menu
 def joueur2_utilise():
-    for i in range (0,4) :
-        combinaison.append(input("choisir une couleur (en anglais)"))
-    print(combinaison)
-    #tentative_joueur()
-
+    afficher_message("Composez le code couleur (joueur 2) :")
+    effacer_choix_mode()
+    afficher_boutons_couleurs()
 
 # programme principal
 
@@ -159,36 +240,10 @@ plateau.grid(row=1, column=1)# positionnement du canvas
 mode_jeu = tk.Canvas(root, background= "wheat1", width = 300, height= 100)# canvas du "mode de jeu"
 mode_jeu.grid(row=0, column=1)# positionnement du canvas
 
-text = mode_jeu.create_text(150, 30, text="Choisissez un mode :")# text qui demande de choisir un mode
-bouton1joueur = tk.Button(root, text="1 joueur", command= joueur1_utilise)# bouton pour jouer à 1 joueur
-bouton2joueur = tk.Button(root, text="2 joueurs", command= joueur2_utilise)# bouton pour jouer à 2 joueurs
-mode_jeu.create_window(100, 70, window=bouton1joueur)# emplacement du bouton "1 joueur"
-mode_jeu.create_window(200, 70, window=bouton2joueur)# emplacement du bouton "2 joueurs"
+afficher_choix_mode()
 # canvas dans lequel il y a les boutons de couleurs
 choix_couleurs = tk.Canvas(root, width= 100, height= 550)
 choix_couleurs.grid(row=1, column=0)
-# boutons de courleurs:
-bouton_red = tk.Button(root, text="rouge", background="red", command= clic_rouge) 
-choix_couleurs.create_window(50, 50, window= bouton_red)
-
-bouton_green = tk.Button(root, text="vert", background="green", command= clic_vert) 
-choix_couleurs.create_window(50, 100, window= bouton_green)
-
-bouton_blue = tk.Button(root, text="bleu", background="blue", command= clic_bleu) 
-choix_couleurs.create_window(50, 150, window= bouton_blue)
-
-bouton_yellow = tk.Button(root, text="jaune", background="yellow", command= clic_jaune) 
-choix_couleurs.create_window(50, 200, window= bouton_yellow)
-
-bouton_orange = tk.Button(root, text="orange", background="orange", command= clic_orange) 
-choix_couleurs.create_window(50, 250, window= bouton_orange)
-
-bouton_purple = tk.Button(root, text="violet", background="purple", command= clic_violet) 
-choix_couleurs.create_window(50, 300, window= bouton_purple)
-# affiche le text "Sélectionnez des couleurs:" dans un canvas apparentière
-canvas_text2 = tk.Canvas(root, width= 100, height= 100)
-canvas_text2.grid(row=0, column=0)
-text2 = canvas_text2.create_text(50, 50, text= "Sélectionnez \ndes couleurs :")
 
 
 root.mainloop()
