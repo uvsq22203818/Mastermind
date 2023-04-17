6e6bc5fa18ea02bb4c50791179d85f148462c815
Romain THOMAS <romain.thomas@ens.uvsq.fr>
2023-04-17 23:37:48 +0200

    retour en arrière

--- Mastermind.py
+++ Mastermind.py
@@ -5,23 +5,52 @@ from json import *
 
 
 # variables
-COULEURS = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
+COULEURS = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'hot pink', 'cyan']
 taille_code = 4
 essais_max = 10
 combinaison = []
-tentative_courante = []
+tentative_courante = [] # tentative
 nbr_tentative = 0
-liste_rond = []
-liste_tentative = []
+liste_rond = [] # liste dans laquelle sera ajouté les ronds de couleurs
+liste_tentative = [] # liste dans laquelle sera ajouté les tentatives de la partie
 
 
+def supprimer_ronds_tentative_courante():
+    global liste_rond
+    for i in range(len(tentative_courante)):
+        rond = liste_rond.pop()
+        plateau.delete(rond)
+
+def supprimer_ronds_tentative_derniere():
+    global liste_rond
+    dernière_tentative = liste_tentative[len(liste_tentative)-1]
+    nb_ronds= 4+dernière_tentative["nb_bienplace"]+dernière_tentative["nb_malplace"]
+    for i in range(nb_ronds):
+        rond = liste_rond.pop()
+        plateau.delete(rond)
+    
+def retour():
+    global nbr_tentative
+    global tentative_courante
+    
+    if tentative_courante == []:
+        if nbr_tentative>0:
+            supprimer_ronds_tentative_derniere()
+            nbr_tentative-=1
+
+    else:
+        supprimer_ronds_tentative_courante()
+        tentative_courante = []  
+    
+#fonction qui supprime tous les ronds de couleurs
 def supprimer_ronds():
     global liste_rond
     for rond in liste_rond:
         plateau.delete(rond)
-    liste_rond =[]
+    liste_rond = []
 
-def affichage_cercle(tentative):
+#fonction qui affiche les cercles
+def affichage_cercle(tentative, tous):
     
     couleur_cercle = 0 #prend la couleurs de la tentative
     #variable cercle
@@ -29,10 +58,11 @@ def affichage_cercle(tentative):
     coor_vert_pt1 = 490-50*(nbr_tentative)
     coor_horiz_pt2 = 40
     coor_vert_pt2 = 460-50*(nbr_tentative)
-    for i in range (len(tentative)) :
-        rond = plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
-                            coor_horiz_pt2, coor_vert_pt2, fill= tentative[couleur_cercle])
-        liste_rond.append(rond)
+    for i in range (len(tentative)) : # affiche les cercles au fur et à mesure que la tentative se s'agrandit
+        if tous == True or i == len(tentative)-1 :
+            rond = plateau.create_oval(coor_horiz_pt1, coor_vert_pt1,\
+                                coor_horiz_pt2, coor_vert_pt2, fill= tentative[couleur_cercle])# affecte le rond à une variable
+            liste_rond.append(rond)
         coor_horiz_pt1 += 40
         coor_horiz_pt2 += 40
         couleur_cercle += 1
@@ -60,16 +90,20 @@ def verification():
                 tentative_courante[k] = "y" # on modifie couleur mal placé pour pas qu'elle soit compté plusieurs fois
                 break # on sort de la boucle une fois qu'une couleur mal placé soit compté une seule fois
                       
+    
+
     cercle_de_vérif(nb_bienplace, nb_malplace)
     nouvelle_tentative = { "nb_bienplace": nb_bienplace, "nb_malplace": nb_malplace, "tentative" :copi_tentative}
     liste_tentative.append(nouvelle_tentative)
    
     if nb_bienplace == 4 :
-        afficher_message("Vous avez gagné !!")
+        effacer_message()
+        afficher_message("Vous avez gagné !!","20")
         effacer_boutons_couleur()
         afficher_bouton_rejouer()
     elif nbr_tentative==10:
-        afficher_message("Vous avez perdu :/")
+        effacer_message()
+        afficher_message("Vous avez perdu :/", "20")
         effacer_boutons_couleur()
         afficher_bouton_rejouer()
     tentative_courante = []
@@ -106,10 +140,12 @@ def clic_bouton(color):
     if len(combinaison)<taille_code:
         combinaison.append(color)
         if len(combinaison)==4:
+            effacer_message()
+            afficher_message("Sélectionnez 4 couleurs\n(jusqu'à trouver le bon code)", "13")
             print(combinaison)
     else:
         tentative_courante.append(color)
-        affichage_cercle(tentative_courante)
+        affichage_cercle(tentative_courante,False)
     
     if len(tentative_courante)==taille_code:
         global nbr_tentative
@@ -132,7 +168,13 @@ def clic_bleu():
     clic_bouton("blue") 
 
 def clic_jaune():
-    clic_bouton("yellow")        
+    clic_bouton("yellow") 
+
+def clic_rose():
+    clic_bouton("hot pink") 
+
+def clic_cyan():
+    clic_bouton("cyan")      
 
 def clic_rejouer():
     supprimer_ronds()
@@ -145,16 +187,17 @@ def clic_rejouer():
     afficher_choix_mode()
     effacer_bouton_rejouer()
     effacer_message()
+    
 
 def effacer_bouton_rejouer():
-    plateau.delete(w_rejouer)
+    mode_jeu.delete(w_rejouer)
         
 
 
 def afficher_bouton_rejouer():
     bouton_rejouer = tk.Button(root, text="rejouer", command= clic_rejouer) 
     global w_rejouer
-    w_rejouer = plateau.create_window(50, 150, window= bouton_rejouer)
+    w_rejouer = mode_jeu.create_window(150, 85, window= bouton_rejouer)
 
 def effacer_boutons_couleur():
     choix_couleurs.delete(w_red)
@@ -163,32 +206,42 @@ def effacer_boutons_couleur():
     choix_couleurs.delete(w_purple)
     choix_couleurs.delete(w_blue)
     choix_couleurs.delete(w_yellow)
+    choix_couleurs.delete(w_cyan)
+    choix_couleurs.delete(w_pink)
 
 def afficher_boutons_couleurs():
     # boutons de courleurs:
     bouton_red = tk.Button(root, text="rouge", background="red", command= clic_rouge) 
     global w_red
-    w_red = choix_couleurs.create_window(50, 150, window= bouton_red)
+    w_red = choix_couleurs.create_window(50, 100, window= bouton_red)
 
     bouton_green = tk.Button(root, text="vert", background="green", command= clic_vert) 
     global w_green
-    w_green= choix_couleurs.create_window(50, 200, window= bouton_green)
+    w_green= choix_couleurs.create_window(50, 150, window= bouton_green)
 
     bouton_blue = tk.Button(root, text="bleu", background="blue", command= clic_bleu) 
     global w_blue
-    w_blue = choix_couleurs.create_window(50, 250, window= bouton_blue)
+    w_blue = choix_couleurs.create_window(50, 200, window= bouton_blue)
 
     bouton_yellow = tk.Button(root, text="jaune", background="yellow", command= clic_jaune) 
     global w_yellow
-    w_yellow = choix_couleurs.create_window(50, 300, window= bouton_yellow)
+    w_yellow = choix_couleurs.create_window(50, 250, window= bouton_yellow)
 
     bouton_orange = tk.Button(root, text="orange", background="orange", command= clic_orange) 
     global w_orange
-    w_orange = choix_couleurs.create_window(50, 350, window= bouton_orange)
+    w_orange = choix_couleurs.create_window(50, 300, window= bouton_orange)
 
     bouton_purple = tk.Button(root, text="violet", background="purple", command= clic_violet) 
     global w_purple
-    w_purple = choix_couleurs.create_window(50, 400, window= bouton_purple)
+    w_purple = choix_couleurs.create_window(50, 350, window= bouton_purple)
+
+    bouton_pink = tk.Button(root, text="rose", background="hot pink", command= clic_rose) 
+    global w_pink
+    w_pink = choix_couleurs.create_window(50, 400, window= bouton_pink)
+
+    bouton_cyan = tk.Button(root, text="cyan", background="cyan", command= clic_cyan) 
+    global w_cyan
+    w_cyan = choix_couleurs.create_window(50, 450, window= bouton_cyan)
     
 
 def effacer_choix_mode():
@@ -209,13 +262,14 @@ def afficher_choix_mode():
     w1 = mode_jeu.create_window(100, 70, window=bouton1joueur)# emplacement du bouton "1 joueur"
     w2 = mode_jeu.create_window(200, 70, window=bouton2joueur)# emplacement du bouton "2 joueurs"
 
-def afficher_message(message):
-    label_message = tk.Label(root, padx=15, pady=30, text= message, font = ("helvetica", "10"))
+def afficher_message(message, taille):
+    label_message = tk.Label(root, padx=25, pady=25, text= message,background= "wheat1", font = ("helvetica", taille))
     global w_label_message
-    w_label_message = mode_jeu.create_window(50, 30, window= label_message)
+    w_label_message = mode_jeu.create_window(150, 50, window= label_message)
 
 def effacer_message():
     mode_jeu.delete(w_label_message)
+    
 
 # Permet de générer un code aléatoirement
 # Fonction appelé par l'utilisation du bouton "1 joueur" dans l'affichage menu
@@ -228,16 +282,18 @@ def joueur1_utilise():
     nbr_tentative= 0
     effacer_choix_mode()
     afficher_boutons_couleurs()
+    afficher_message("Sélectionnez 4 couleurs\n(jusqu'à trouver le bon code)", "13")
+
 # Permet à un deuxième joueur de choisir un code secret.
 # Fonction appelé par l'utilisation du bouton "2 joueur" dans l'affichage menu
 def joueur2_utilise():
-    afficher_message("Composez le code couleur (joueur 2) :")
+    afficher_message("Composez le code couleur (joueur 2) :", "10")
     effacer_choix_mode()
     afficher_boutons_couleurs()
 
 def sauvegarder():
-    print(liste_tentative)
     fichier = open ("./mastermind.json","w")
+    global dic
     dic = {"liste_tentative": liste_tentative, "combinaison": combinaison}
     dump(dic, fichier, indent=4)
     fichier.close()
@@ -247,22 +303,23 @@ def restaurer():
     strjson=fichier.read()
     fichier.close()
     dic = loads(strjson)
-    print (liste_tentative)
     global nbr_tentative
     nbr_tentative = 0
+    # restauration de la partie
     for tentative in dic["liste_tentative"]:
-        affichage_cercle(tentative["tentative"])
+        affichage_cercle(tentative["tentative"], True)
         nbr_tentative+=1
         cercle_de_vérif(tentative["nb_bienplace"], tentative["nb_malplace"])
     global combinaison
     combinaison = dic["combinaison"]
     afficher_boutons_couleurs()
+    
 
 # programme principal
 
 root = tk.Tk()# création de la fenêtre dans laquelle nous jouerons au "mastermind"
 root.title("Mastermind")# titre donné à la fenêtre
-root.geometry("400x600")
+root.geometry("400x600+10+10")
 plateau = tk.Canvas(root,background= "salmon4",width = 300, height = 550)# canvas où s'affichera les pions de couleurs
 plateau.grid(row=1, column=1)# positionnement du canvas
 # affichage du menu
@@ -270,17 +327,25 @@ mode_jeu = tk.Canvas(root, background= "wheat1", width = 300, height= 100)# canv
 mode_jeu.grid(row=0, column=1)# positionnement du canvas
 
 afficher_choix_mode()
+
 # canvas dans lequel il y a les boutons de couleurs
 choix_couleurs = tk.Canvas(root, width= 100, height= 550)
 choix_couleurs.grid(row=1, column=0)
 
+# canvas dans lequel il y a les boutons de sauvegarde, restauration, retour en arrière
 canvas_fichier = tk.Canvas(root, width = 100, height= 100)
 canvas_fichier.grid(row=0, column=0)# positionnement du canvas
 
+# bouton pour sauvegarder
 bouton_sauvegarder = tk.Button(root, text="sauvegarder", command= sauvegarder ) 
-w_sauvegarder = canvas_fichier.create_window(50, 10, window= bouton_sauvegarder)
+w_sauvegarder = canvas_fichier.create_window(50, 20, window= bouton_sauvegarder)
 
+# bouton pour restaurer
 bouton_restaurer = tk.Button(root, text="restaurer", command= restaurer ) 
-w_restaurer = canvas_fichier.create_window(50, 60, window= bouton_restaurer)
+w_restaurer = canvas_fichier.create_window(50, 50, window= bouton_restaurer)
+
+# bouton pour revenir en arrière
+bouton_retour = tk.Button(root, text="retour", command= retour ) 
+w_retour = canvas_fichier.create_window(50, 80, window= bouton_retour)
 
 root.mainloop()
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Nom du projet: mastermind 
 Groupe de TD: BI 3
 Groupe projet: ?
-Membres du groupe Projet: THOMAS Romain, GRAND Sébastien, Thérésa, David
+Membres du groupe Projet: THOMAS Romain, GRAND Sébastien, Thereza, JAMES David
